600e7304c63b6cd9cd448222c5601d2ad28432fc
Darwin Bautista <darwin.bautista@eee.upd.edu.ph>
2018-09-01 19:08:37 +0800

    Add VIGNet-specific changes

--- render_blender.py
+++ render_blender.py
@@ -33,58 +33,60 @@ args = parser.parse_args(argv)
 import bpy
 
 # Set up rendering of depth map.
-bpy.context.scene.use_nodes = True
-tree = bpy.context.scene.node_tree
-links = tree.links
+#bpy.context.scene.use_nodes = True
+#tree = bpy.context.scene.node_tree
+#links = tree.links
 
 # Add passes for additionally dumping albedo and normals.
-bpy.context.scene.render.layers["RenderLayer"].use_pass_normal = True
-bpy.context.scene.render.layers["RenderLayer"].use_pass_color = True
-bpy.context.scene.render.image_settings.file_format = args.format
-bpy.context.scene.render.image_settings.color_depth = args.color_depth
+#bpy.context.scene.render.layers["RenderLayer"].use_pass_normal = True
+#bpy.context.scene.render.layers["RenderLayer"].use_pass_color = True
+#bpy.context.scene.render.image_settings.file_format = args.format
+#bpy.context.scene.render.image_settings.color_depth = args.color_depth
 
 # Clear default nodes
-for n in tree.nodes:
-    tree.nodes.remove(n)
+#for n in tree.nodes:
+#    tree.nodes.remove(n)
 
 # Create input render layer node.
-render_layers = tree.nodes.new('CompositorNodeRLayers')
+#render_layers = tree.nodes.new('CompositorNodeRLayers')
 
-depth_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
-depth_file_output.label = 'Depth Output'
+#depth_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+#depth_file_output.label = 'Depth Output'
 if args.format == 'OPEN_EXR':
-  links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
+  #links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
+  pass
 else:
   # Remap as other types can not represent the full range of depth.
-  map = tree.nodes.new(type="CompositorNodeMapValue")
+  #map = tree.nodes.new(type="CompositorNodeMapValue")
   # Size is chosen kind of arbitrarily, try out until you're satisfied with resulting depth map.
-  map.offset = [-0.7]
-  map.size = [args.depth_scale]
-  map.use_min = True
-  map.min = [0]
-  links.new(render_layers.outputs['Depth'], map.inputs[0])
+  #map.offset = [-0.7]
+  #map.size = [args.depth_scale]
+  #map.use_min = True
+  #map.min = [0]
+  pass
+  #links.new(render_layers.outputs['Depth'], map.inputs[0])
 
-  links.new(map.outputs[0], depth_file_output.inputs[0])
+  #links.new(map.outputs[0], depth_file_output.inputs[0])
 
-scale_normal = tree.nodes.new(type="CompositorNodeMixRGB")
-scale_normal.blend_type = 'MULTIPLY'
+#scale_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+#scale_normal.blend_type = 'MULTIPLY'
 # scale_normal.use_alpha = True
-scale_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
-links.new(render_layers.outputs['Normal'], scale_normal.inputs[1])
+#scale_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
+#links.new(render_layers.outputs['Normal'], scale_normal.inputs[1])
 
-bias_normal = tree.nodes.new(type="CompositorNodeMixRGB")
-bias_normal.blend_type = 'ADD'
-# bias_normal.use_alpha = True
-bias_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
-links.new(scale_normal.outputs[0], bias_normal.inputs[1])
+#bias_normal = tree.nodes.new(type="CompositorNodeMixRGB")
+#bias_normal.blend_type = 'ADD'
+## bias_normal.use_alpha = True
+#bias_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
+#links.new(scale_normal.outputs[0], bias_normal.inputs[1])
 
-normal_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
-normal_file_output.label = 'Normal Output'
-links.new(bias_normal.outputs[0], normal_file_output.inputs[0])
+#normal_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+#normal_file_output.label = 'Normal Output'
+#links.new(bias_normal.outputs[0], normal_file_output.inputs[0])
 
-albedo_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
-albedo_file_output.label = 'Albedo Output'
-links.new(render_layers.outputs['Color'], albedo_file_output.inputs[0])
+#albedo_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
+#albedo_file_output.label = 'Albedo Output'
+#links.new(render_layers.outputs['Color'], albedo_file_output.inputs[0])
 
 # Delete default cube
 bpy.data.objects['Cube'].select = True
@@ -137,8 +139,8 @@ def parent_obj_to_camera(b_camera):
 
 
 scene = bpy.context.scene
-scene.render.resolution_x = 600
-scene.render.resolution_y = 600
+scene.render.resolution_x = 128
+scene.render.resolution_y = 128
 scene.render.resolution_percentage = 100
 scene.render.alpha_mode = 'TRANSPARENT'
 cam = scene.objects['Camera']
@@ -149,7 +151,8 @@ cam_constraint.up_axis = 'UP_Y'
 b_empty = parent_obj_to_camera(cam)
 cam_constraint.target = b_empty
 
-model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
+model_identifier = os.path.basename(os.path.split(os.path.split(args.obj)[0])[0])
+#print((os.path.split(args.obj)))
 fp = os.path.join(args.output_folder, model_identifier, model_identifier)
 scene.render.image_settings.file_format = 'PNG'  # set output format to .png
 
@@ -158,17 +161,28 @@ from math import radians
 stepsize = 360.0 / args.views
 rotation_mode = 'XYZ'
 
-for output_node in [depth_file_output, normal_file_output, albedo_file_output]:
-    output_node.base_path = ''
+#for output_node in [depth_file_output, normal_file_output, albedo_file_output]:
+#    output_node.base_path = ''
+
+import random
+import math
+
+b_empty.rotation_euler[1] = radians(random.randint(0, 91))
+
+
+scale_range = math.sqrt(2) - 1
 
 for i in range(0, args.views):
     print("Rotation {}, {}".format((stepsize * i), radians(stepsize * i)))
 
     scene.render.filepath = fp + '_r_{0:03d}'.format(int(i * stepsize))
-    depth_file_output.file_slots[0].path = scene.render.filepath + "_depth.png"
-    normal_file_output.file_slots[0].path = scene.render.filepath + "_normal.png"
-    albedo_file_output.file_slots[0].path = scene.render.filepath + "_albedo.png"
+    #depth_file_output.file_slots[0].path = scene.render.filepath + "_depth.png"
+    #normal_file_output.file_slots[0].path = scene.render.filepath + "_normal.png"
+    #albedo_file_output.file_slots[0].path = scene.render.filepath + "_albedo.png"
 
     bpy.ops.render.render(write_still=True)  # render still
 
     b_empty.rotation_euler[2] += radians(stepsize)
+    b_empty.rotation_euler[1] = radians(random.randint(0, 91))
+    s = scale_range * random.random() + 1
+    cam.location = (0, s, 0.6 * s)
